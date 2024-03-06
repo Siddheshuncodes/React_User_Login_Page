63c6efc094136340dc59c42c339d5e90b1fca500
CoolShivas <shivas2710cool00@gmail.com>
2024-03-06 10:07:21 +0530

    Tapping_Into_Context_with_useContext_Hook

--- user_login_page/src/components/MainHeader/Navigation.js
+++ user_login_page/src/components/MainHeader/Navigation.js
@@ -1,34 +1,30 @@
-
+import { useContext } from 'react';
 import AuthContext from '../../store/AuthContext';
 import classes from './Navigation.module.css';
 
 const Navigation = (props) => {
-    return (
-        <AuthContext.Consumer>
-            {(xyz) => {
-                return <nav className={classes.nav}>
-                    <ul>
-                        {xyz.isLoggedIn && (
-                            <li>
-                                <a href="/">Users</a>
-                            </li>
-                        )}
-                        {xyz.isLoggedIn && (
-                            <li>
-                                <a href="/">Admin</a>
-                            </li>
-                        )}
-                        {xyz.isLoggedIn && (
-                            <li>
-                                <button onClick={props.onLogout}>Logout</button>
-                            </li>
-                        )}
-                    </ul>
-                </nav>
-            }}
 
-        </AuthContext.Consumer>
-    );
+    const xyz = useContext(AuthContext);
+
+    return <nav className={classes.nav}>
+        <ul>
+            {xyz.isLoggedIn && (
+                <li>
+                    <a href="/">Users</a>
+                </li>
+            )}
+            {xyz.isLoggedIn && (
+                <li>
+                    <a href="/">Admin</a>
+                </li>
+            )}
+            {xyz.isLoggedIn && (
+                <li>
+                    <button onClick={props.onLogout}>Logout</button>
+                </li>
+            )}
+        </ul>
+    </nav>
 };
 
 export default Navigation;
